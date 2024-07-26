f2f3b9223828a28ef3a8940b7136a5e69f66e168
Gizem EROL <gizerol@hotmail.com>
2024-07-26 22:06:54 +0300

    Updated sql

--- src/config.py
+++ src/config.py
@@ -7,4 +7,5 @@ class JwtSettings:
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'mssql+pymssql://sqlserver:JzOTAk-}h"jPpBC0@35.242.208.225/Deneme'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     API_KEY = os.environ.get('API_KEY')
